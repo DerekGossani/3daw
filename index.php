e85b204d1f0149299ad1b984bd02c42fcbb0f548
DerekGossani <derekgossani605@gmail.com>
2024-09-24 11:42:23 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste de Perguntas e Respostas</title>
+</head>
+<body>
+    <h1>Bem-vindo ao Teste de Perguntas e Respostas</h1>
+    <ul>
+        <li><a href="cadastroPerguntas.php">Criar Pergunta de Múltipla Escolha</a></li>
+        <li><a href="listarPerguntas.php">Listar Perguntas</a></li>
+    </ul>
+</body>
+</html>
